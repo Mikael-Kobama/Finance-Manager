da38e947af0048cd01f730102b470cdc404dde39
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-09-07 23:52:30 -0300

    update transaction reader

---
+++ backend/src/main/java/br/com/mikaelkobama/backend/Transacao.java
@@ -0,0 +1,10 @@
+package br.com.mikaelkobama.backend;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Time;
+
+public record Transacao(Long id, Integer tipo, Date data, BigDecimal valor, Long cpf, String cartao,
+    Time hora, String donoDaLoja, String nomeDaLoja) {
+}
+
---
+++ backend/src/main/java/br/com/mikaelkobama/backend/TransacaoCNAB.java
@@ -0,0 +1,8 @@
+package br.com.mikaelkobama.backend;
+
+import java.math.BigDecimal;
+
+public record TransacaoCNAB(Integer tipo, String data, BigDecimal valor, Long cpf, String cartao,
+    String hora, String donoDaLoja, String nomeDaLoja) {
+
+}
